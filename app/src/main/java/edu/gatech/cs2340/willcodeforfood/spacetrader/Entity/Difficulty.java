754cab733fd22253c0a4417848d92182c7ddf80b
Matthew Bernet <mattbernetGT@gmail.com>
2019-02-16 15:33:04 -0500

    Entity class updates

--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/Difficulty.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/Difficulty.java
@@ -1,5 +1,29 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.Entity;
 
+/**
+ * Game Difficulty
+ *
+ * @author Matt Bernet
+ * @version 1.0
+ */
 public enum Difficulty {
-    //Game difficulty implementation
+    BEGINNER("Beginner"),
+    EASY("Easy"),
+    NORMAL("Normal"),
+    HARD("Hard"),
+    IMPOSSIBLE("Impossible");
+
+    public String difficulty;
+
+    /**
+     * Constructor for Difficulty enumeration
+     *
+     * @param diff difficulty
+     */
+    Difficulty(String diff) {
+        difficulty = diff;
+    }
+
+    @Override
+    public String toString() {return difficulty;}
 }
--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/Gnat.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/Gnat.java
@@ -1,5 +1,17 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.Entity;
 
+/**
+ * Represents a Gnat Ship
+ *
+ * @author Matt Bernet
+ * @version 1.0
+ */
 public class Gnat extends Ship {
-    //Gnat spaceship implementation
+
+    /**
+     * Initializes a Gnat Ship
+     */
+    public Gnat() {
+        super("Gnat", "black", 300);
+    }
 }
