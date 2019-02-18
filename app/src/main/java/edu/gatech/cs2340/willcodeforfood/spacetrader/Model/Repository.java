cddeb41aa53bf0597c70910ec7629db3f88222cb
Matthew Bernet <mattbernetGT@gmail.com>
2019-02-18 16:52:12 -0500

    Player creation log

--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Model/Repository.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Model/Repository.java
@@ -1,5 +1,7 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.Model;
 
+import android.util.Log;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -29,6 +31,7 @@ public class Repository {
      */
     public void addPlayer(Player player) {
         players.add(player);
+        Log.w("Add", "New player added: " + player.toString());
     }
 
     /**
