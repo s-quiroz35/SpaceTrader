093a53f3c8b65ae11c763253120fd190ac9779dc
Matthew Bernet <mattbernetGT@gmail.com>
2019-02-21 15:11:08 -0500

    Created solar system class

---
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/SolarSystem.java
@@ -0,0 +1,11 @@
+package edu.gatech.cs2340.willcodeforfood.spacetrader.Entity;
+
+/**
+ * Represents a Solar System
+ *
+ * @author Matt Bernet
+ * @version 1.0
+ */
+public class SolarSystem {
+
+}
