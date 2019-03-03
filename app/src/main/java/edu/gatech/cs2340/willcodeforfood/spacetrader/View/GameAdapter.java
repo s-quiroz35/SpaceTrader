936ce5434d3e1498da5f888a982d7319b1b4169f
Matthew Bernet <mattbernetGT@gmail.com>
2019-03-02 19:30:18 -0500

    Games list screen updates

--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/GameAdapter.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/GameAdapter.java
@@ -5,9 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.gatech.cs2340.willcodeforfood.spacetrader.Entity.Game;
@@ -18,20 +18,29 @@ import edu.gatech.cs2340.willcodeforfood.spacetrader.R;
  * Adapts games to recycler view
  *
  * @author Matt Bernet
- * @version 1.0
+ * @version 1.1
  */
 public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
 
-    private List<Game> games = new ArrayList<>();
-    private OnGameClickListener listener;
+    private List<Game> games;
+    private GameClickListener listener;
+
+    /**
+     * Initializes Game Adapter
+     *
+     * @param games all games
+     * @param listener click listener
+     */
+    GameAdapter(List<Game> games, GameClickListener listener) {
+        this.games = games;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.player_item, parent, false);
-
-        return new GameViewHolder(itemView);
+        return new GameViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.player_item, parent, false));
     }
 
     @Override
@@ -44,52 +53,37 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder
     public int getItemCount() { return games.size(); }
 
     /**
-     * Set games from model
-     *
-     * @param games list of games
-     */
-    public void setGames(List<Game> games) {
-        this.games = games;
-        notifyDataSetChanged();
-    }
-
-    /**
-     * Sets a listener for game click activity
-     *
-     * @param listener new listener
-     */
-    public void setOnGameClickListener(OnGameClickListener listener) {
-        this.listener = listener;
-    }
-
-    /**
      * Holder for single game in list
      */
-    class GameViewHolder extends RecyclerView.ViewHolder {
+    class GameViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView playerName;
+        private ImageView editIcon;
 
         GameViewHolder(@NonNull View itemView) {
             super(itemView);
             playerName = itemView.findViewById(R.id.player_name);
+            editIcon = itemView.findViewById(R.id.edit_player_icon);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int position = getAdapterPosition();
+            itemView.setOnClickListener(this);
+            editIcon.setOnClickListener(this);
+        }
 
-                    if (listener != null && position != RecyclerView.NO_POSITION) {
-                        listener.onGameClicked(games.get(position));
-                    }
-                }
-            });
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == editIcon.getId()) {
+                listener.onEditClick(games.get(getAdapterPosition()));
+            } else {
+                listener.onGameClick(games.get(getAdapterPosition()));
+            }
         }
     }
 
     /**
-     * Interface with method to be implemented in parent activity for click response
+     * Interface with methods to be implemented in parent activity for click response
      */
-    public interface OnGameClickListener {
-        void onGameClicked(Game game);
+    public interface GameClickListener {
+        void onGameClick(Game game);
+        void onEditClick(Game game);
     }
 }
 
--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/ViewAllGamesActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/ViewAllGamesActivity.java
@@ -1,14 +1,12 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.View;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
-import android.view.View;
 
 import edu.gatech.cs2340.willcodeforfood.spacetrader.Entity.Game;
 import edu.gatech.cs2340.willcodeforfood.spacetrader.R;
@@ -17,7 +15,7 @@ import edu.gatech.cs2340.willcodeforfood.spacetrader.ViewModel.GameListingViewMo
 /**
  * View all games
  * @author Matt Bernet
- * @version 1.0
+ * @version 1.1
  */
 public class ViewAllGamesActivity extends AppCompatActivity {
 
@@ -29,28 +27,24 @@ public class ViewAllGamesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_player_list);
 
+        viewModel = ViewModelProviders.of(this).get(GameListingViewModel.class);
+
         RecyclerView rView = findViewById(R.id.players_list);
         LinearLayoutManager manager = new LinearLayoutManager(this);
         rView.setLayoutManager(manager);
-        adapter = new GameAdapter();
+        adapter = new GameAdapter(viewModel.getGames(), new GameAdapter.GameClickListener() {
+            @Override
+            public void onGameClick(Game game) {
+                Log.w("Click", "Loading game! " + game.getPlayer().getName());
+            }
+            @Override
+            public void onEditClick(Game game) {
+                Log.w("Click", "Editing player! " + game.getPlayer().getName());
+            }
+        });
         rView.setAdapter(adapter);
         DividerItemDecoration divider = new DividerItemDecoration(rView.getContext(),
                 manager.getOrientation());
         rView.addItemDecoration(divider);
-
-        viewModel = ViewModelProviders.of(this).get(GameListingViewModel.class);
-        adapter.setGames(viewModel.getGames());
-        adapter.setOnGameClickListener(new GameAdapter.OnGameClickListener() {
-            @Override
-            public void onGameClicked(Game game) {
-                Log.w("GameClick", String.format("Player %s clicked, loading game!",
-                        game.getPlayer().getName()));
-            }
-        });
-    }
-
-    public void onEditPressed(View view) {
-        //Intent intent = new Intent(ViewAllGamesActivity.this, ConfigActivity.class);
-        //intent.putExtra("Game", game)
     }
 }
