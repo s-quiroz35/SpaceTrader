db022ba9aa39771b4c38e46b88211f64709371b1
Matthew Bernet <mattbernetGT@gmail.com>
2019-03-02 20:22:23 -0500

    Added project entities/activities

---
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/Entity/Good.java
@@ -0,0 +1,5 @@
+package edu.gatech.cs2340.willcodeforfood.spacetrader.Entity;
+
+public class Good {
+    //good implementation
+}
--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/PlanetActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/PlanetActivity.java
@@ -2,6 +2,6 @@ package edu.gatech.cs2340.willcodeforfood.spacetrader.View;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class UniverseActivity extends AppCompatActivity {
-    //universe activity implementation
+public class PlanetActivity extends AppCompatActivity {
+    //planet activity implementation
 }
