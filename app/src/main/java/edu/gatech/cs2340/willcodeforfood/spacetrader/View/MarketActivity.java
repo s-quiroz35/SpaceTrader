48923e97aae7cfe17b5efe42fb85a407bbb51d7a
Matthew Bernet <mattbernetGT@gmail.com>
2019-03-08 11:03:09 -0500

    Started market layout

--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/MarketActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/MarketActivity.java
@@ -1,7 +1,30 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.View;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import edu.gatech.cs2340.willcodeforfood.spacetrader.R;
+
+/**
+ * Handles market activity
+ *
+ * @author Matt Bernet
+ * @version 1.0
+ */
 public class MarketActivity extends AppCompatActivity {
-    //market activity implementation
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_market);
+    }
+
+    public void onBuyPressed(View view) {
+
+    }
+
+    public void onSellPressed(View view) {
+
+    }
 }
--- app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/PlanetActivity.java
+++ app/src/main/java/edu/gatech/cs2340/willcodeforfood/spacetrader/View/PlanetActivity.java
@@ -1,7 +1,33 @@
 package edu.gatech.cs2340.willcodeforfood.spacetrader.View;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import edu.gatech.cs2340.willcodeforfood.spacetrader.R;
+
+/**
+ * Handles planet activity
+ *
+ * @author Matt Bernet
+ * @version 1.0
+ */
 public class PlanetActivity extends AppCompatActivity {
-    //planet activity implementation
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_planet);
+    }
+
+    /**
+     * Button handler for entering market
+     *
+     * @param view button pressed
+     */
+    public void onMarketPressed(View view) {
+        startActivity(new Intent(PlanetActivity.this, MarketActivity.class));
+    }
 }
